c57ad0084da198e3eb6a2776c2f1a10469b4a987
Vladimir Avdeev <vahundos@gmail.com>
2017-10-06 23:44:32 +0300

    Note entity class createad
    createad note entity class and added maven dependency for spring-orm and hibernate

---
+++ src/main/java/com/vahundos/javarush/testtask/note/entity/Note.java
@@ -0,0 +1,66 @@
+package com.vahundos.javarush.testtask.note.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "note")
+public class Note {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "done")
+    private boolean done;
+
+    @Column(name = "createdDate")
+    private LocalDateTime createdDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+}
