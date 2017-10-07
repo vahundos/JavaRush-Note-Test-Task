faa3e04f8d1ea3aa063e4fabae94cea693586045
Vladimir Avdeev <vahundos@gmail.com>
2017-10-07 15:43:43 +0300

    db dependencies and data source in AppConifg

--- src/main/java/com/vahundos/javarush/testtask/note/configuration/AppConfig.java
+++ src/main/java/com/vahundos/javarush/testtask/note/configuration/AppConfig.java
@@ -1,23 +1,43 @@
 package com.vahundos.javarush.testtask.note.configuration;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.vahundos.javarush.testtask.note")
+@PropertySource("classpath:db.properties")
 public class AppConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
-    public ViewResolver viewResolver() {
+    public ViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setPrefix(".jsp");
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
     }
 
 }
