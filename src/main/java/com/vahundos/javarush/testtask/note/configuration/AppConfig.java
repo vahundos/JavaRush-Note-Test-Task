766083373d131cd198db8af50eeb41328521391e
Vladimir Avdeev <vahundos@gmail.com>
2017-10-07 18:07:14 +0300

    bug-fix and adding transactionManager
    *double setPrefix for viewResolver
    *require hibernate.dialect

--- src/main/java/com/vahundos/javarush/testtask/note/configuration/AppConfig.java
+++ src/main/java/com/vahundos/javarush/testtask/note/configuration/AppConfig.java
@@ -3,22 +3,23 @@ package com.vahundos.javarush.testtask.note.configuration;
 import com.vahundos.javarush.testtask.note.entity.Note;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.vahundos.javarush.testtask.note")
 @PropertySource("classpath:db.properties")
+@EnableTransactionManagement
 public class AppConfig {
 
     @Autowired
@@ -28,7 +29,7 @@ public class AppConfig {
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/view/");
-        viewResolver.setPrefix(".jsp");
+        viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
 
@@ -47,8 +48,21 @@ public class AppConfig {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
 
+        Properties props = new Properties();
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        factoryBean.setHibernateProperties(props);
         factoryBean.setAnnotatedClasses(Note.class);
         return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
     }
 
 }
