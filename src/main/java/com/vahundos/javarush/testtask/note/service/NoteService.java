c82879c882b46f95c38280fb2076ea89fc021f65
Vladimir Avdeev <vahundos@gmail.com>
2017-10-07 16:45:02 +0300

    added NoteService and his impl

---
+++ src/main/java/com/vahundos/javarush/testtask/note/service/NoteService.java
@@ -0,0 +1,13 @@
+package com.vahundos.javarush.testtask.note.service;
+
+import com.vahundos.javarush.testtask.note.entity.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    void add(Note note);
+    void update(Note note);
+    void remove(long id);
+    Note getNoteById(long id);
+    List<Note> getAllNotes();
+}
---
+++ src/main/java/com/vahundos/javarush/testtask/note/service/NoteServiceImp.java
@@ -0,0 +1,46 @@
+package com.vahundos.javarush.testtask.note.service;
+
+import com.vahundos.javarush.testtask.note.dao.NoteDao;
+import com.vahundos.javarush.testtask.note.entity.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class NoteServiceImp implements NoteService {
+
+    @Autowired
+    private NoteDao noteDao;
+
+    @Transactional
+    @Override
+    public void add(Note note) {
+        noteDao.add(note);
+    }
+
+    @Transactional
+    @Override
+    public void update(Note note) {
+        noteDao.update(note);
+    }
+
+    @Transactional
+    @Override
+    public void remove(long id) {
+        noteDao.remove(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Note getNoteById(long id) {
+        return noteDao.getNoteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Note> getAllNotes() {
+        return noteDao.getAllNotes();
+    }
+}
